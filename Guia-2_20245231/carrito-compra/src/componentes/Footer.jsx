f70a9c328c1c49075a1e66b54c1ba2c57eaf15ee
ndiaznuila13 <napoleondiaznuilacerritos@gmail.com>
2026-01-24 14:16:30 -0600

    Ejercicio 5 completo

---
+++ Guia-2_20245231/carrito-compra/src/componentes/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export const Footer = () => {
+  return (
+    <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+  )
+}
+
